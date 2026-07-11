5d641e803ba53a166aefe49ac35d762b9757ad18
Al Farizi <zetsura.official04@gmail.com>
2026-07-11 14:19:41 +0700

    feat(api): integrate app module with prisma, user. role and config module, add support for zod

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { RolesModule } from './modules/roles/roles.module';
+// import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ConfigModule.forRoot(), UsersModule, RolesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
